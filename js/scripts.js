45d07628280b2cbc2925d9d888512f3926ef45c7
btrischuk <b-trischuk@hotmail.com>
2017-10-16 22:40:38 -0700

    add scroll targer js

--- js/scripts.js
+++ js/scripts.js
@@ -28,3 +28,17 @@ $('.add-cart').click(function() {
   $('.counter').html(count);
 
 });
+
+// adjust scroll target https://gist.github.com/HoundstoothSTL/5510082
+
+$('a[href*="#"]').not('a[href="#"]').on(function() {
+  var target = $(this.hash),
+  headerHeight = $("header").outerHeight(); // Get fixed header height
+        
+  if (target.length) {
+    $('html,body').animate({
+      scrollTop: target.offset().top - headerHeight
+    }, 500);
+    return false;
+  }
+});
